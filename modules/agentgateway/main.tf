342c2b01d70d30b7ae93075ea7b4e07e598f1784
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-12-01 00:27:35 +0200

    Update main.tf

--- modules/agentgateway/main.tf
+++ modules/agentgateway/main.tf
@@ -1,4 +1,22 @@
 
+
+resource "null_resource" "gateway_api_crds" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/standard-install.yaml
+      kubectl wait --for condition=established --timeout=90s crd/gatewayclasses.gateway.networking.k8s.io
+    EOT
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<-EOT
+      kubectl delete -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/standard-install.yaml --ignore-not-found=true
+    EOT
+  }
+}
+
+
 resource "helm_release" "kgateway_crds" {
   name              = "kgateway-crds"
   namespace         = "kgateway-system"
@@ -12,18 +30,46 @@ resource "helm_release" "kgateway_crds" {
   cleanup_on_fail = true
   timeout         = 300
 
+  #depends_on = [null_resource.gateway_api_crds]
 }
 
-resource "helm_release" "agentgateway" {
-  depends_on = [helm_release.kgateway_crds]
 
-  name             = "agentgateway"
-  repository       = "https://kgateway.github.io/helm-charts"
-  chart            = "agentgateway"
-  namespace        = "kgateway"
-  create_namespace = true
+resource "helm_release" "kgateway" {
+  name      = "kgateway"
+  namespace = "kgateway-system"
+
+  chart   = "oci://cr.kgateway.dev/kgateway-dev/charts/kgateway"
+  version = "v2.1.1"
+  wait            = true
+  atomic          = true
+  cleanup_on_fail = true
+  timeout         = 600
+  #values = [
+  #  file("${path.module}/kgateway.yaml")
+  #]
+
+  set {
+    name  = "agentgateway.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "gateway.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "controller.replicas"
+    value = "2"
+  }
+
+  set {
+    name  = "envoyProxy.service.type"
+    value = "LoadBalancer"
+  }
 
   depends_on = [
+    null_resource.gateway_api_crds,
     helm_release.kgateway_crds
   ]
 }
